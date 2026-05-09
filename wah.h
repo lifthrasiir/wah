1dce0ae554f96d30e4355921ed9904757e31abbd
Kang Seonghoon <public+git@mearie.org>
2026-05-09 23:52:59 +0900

    Version 2635.

--- wah.h
+++ wah.h
@@ -1,5 +1,5 @@
 // ()  ()   ()   ()  ()  WebAssembly interpreter in a Header file (WAH)
-// ()()() ()()() ()()()  Kang Seonghoon, version 2630 (2026-05), Public Domain
+// ()()() ()()() ()()()  Kang Seonghoon, version 2635 (2026-05), Public Domain
 //  ()()  ()  () ()  ()  https://github.com/lifthrasiir/wah
 
 #ifndef WAH_H
@@ -23,7 +23,7 @@ extern "C" {
 // Macro: WAH_VERSION
 //   The version of the WAH API. Incremented on any change to the API, including bug fixes.
 //   Based on (fractional Gregorian year - 2000) * 100, with a liberal rounding.
-#define WAH_VERSION -2630
+#define WAH_VERSION 2635
 
 // Macro: WAH_FORCE_PORTABLE [user-definable]
 //   If defined, forces the interpreter to use portable C implementations
